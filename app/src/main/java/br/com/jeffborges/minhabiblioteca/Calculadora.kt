48e95c07ccafd198ea12cf1ae9317f641133788e
logonrm <logonrm@fiap.com.br>
2018-03-26 21:05:18 -0300

    Initial commit

---
+++ app/src/main/java/br/com/jeffborges/minhabiblioteca/Calculadora.kt
@@ -0,0 +1,7 @@
+package br.com.jeffborges.minhabiblioteca
+
+class Calculadora(){
+    fun somar(v1: Int, v2: Int): Int{
+        return v1 + v2
+    }
+}
---
+++ app/src/main/java/br/com/jeffborges/minhabiblioteca/CustomToast.java
@@ -0,0 +1,26 @@
+package br.com.jeffborges.minhabiblioteca;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class CustomToast {
+    public void showToast(Context context, String message){
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View customToastRoot = inflater.inflate(R.layout.custom_toast, null);
+
+        Toast customToast = new Toast(context);
+
+        customToast.setView(customToastRoot);
+        TextView textView = customToastRoot.findViewById(R.id.textView1);
+        textView.setText(message);
+        customToast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM, 0, 0);
+        customToast.setDuration(Toast.LENGTH_LONG);
+        customToast.show();
+    }
+}
